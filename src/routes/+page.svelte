e58b51e72497b618c6d5a54a5a315c8c5cffe859
Nathan <nathan.perrine@gmail.com>
2023-11-08 17:12:37 -0500

    main layout, body bg z fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,5 @@
-<h1 class="text-3xl font-bold underline">
-  <p class='text-green-400'>
-    Hello world!!
-  </p>
-</h1>
+<script>
+  import Main from '../components/Main.svelte'
+</script>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.300);
-}
-</style>
+<Main />
