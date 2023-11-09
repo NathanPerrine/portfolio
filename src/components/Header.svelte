f568c0c1ed514f0156adc9b71516026fc0e2165e
Nathan <nathan.perrine@gmail.com>
2023-11-09 01:00:30 -0500

    main started

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -27,6 +27,6 @@
   </div>
   <a href="https://www.linkedin.com/in/nathan-perrine/" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950">
     <div class="absolute top-0 right-full w-full h-full bg-slate-950 opacity-20 group-hover:translate-x-full z-10 duration-200"></div>
-    <h4 class="relative z-9">Get in touch</h4>
+    <h4 class="relative z-9 poppins">Get in touch</h4>
   </a>
 </header>
