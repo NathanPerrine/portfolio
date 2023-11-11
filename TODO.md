c5c740db60b47a3795adeb18c1416205bf5ec914
Nathan <nathan.perrine@gmail.com>
2023-11-11 00:28:58 -0500

    pointer-events-auto/none on to top button/div

--- TODO.md
+++ TODO.md
@@ -1,10 +1,14 @@
-[ ]get in touch button - component
-[ ]steps - component, doesn't seem complete
-[ ] update about me
+* [ ] get in touch button - component
+* [ ] steps - component, doesn't seem complete
+* [ ] update about me
 * to top button
   * [ ] can't be clicked on after about section starts
   * [ ] z-index doesn't show over all other buttons
     * [x] go to -> button under projects
       * remvoing with the 'position: relative' on the go to buttons let them fall down the z-index again
-    * [ ] about me entire section
+    * [x] about me entire section
+    * [ ] want to know more section
   * [ ] disappears on wider resolutions
+* [ ] full screen - weird purple border on left, to top button disappears
+  * go to ->, hovering over that has large purple window...
+    * possibly from removing 'relative' above
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,14 +14,14 @@
 <div
   class="relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen"
 >
-  <div class={"pt-0 fixed bottom-0 w-full duration-200 flex p-10 " +
+  <div class={"pt-0 pointer-events-none fixed bottom-0 w-full duration-200 flex p-10 " +
     (y > 0
-      ? " opacity-full pointer-events-auto"
-      : " pointer-events-none opacity-0")}
+      ? " opacity-full "
+      : "  opacity-0")}
   >
     <button
       on:click={goTop}
-      class="z-10 ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
+      class="z-10 pointer-events-auto ml-auto rounded-full bg-slate-900 text-violet-400 px-3 sm:px-4 hover:bg-slate-800 cursor-pointer aspect-square grid place-items-center"
     >
       <i class="fa-solid fa-arrow-up" />
     </button>
